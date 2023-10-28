f69b28582bef7032ad8124fc133bbdc1d453a18b
Fatwaff <fax.mp4@gmail.com>
2023-10-28 11:51:16 +0700

    update proposal.js

--- js/proposal.js
+++ js/proposal.js
@@ -1,4 +1,7 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { isiDataProposal } from "./getKonten.js";
 
-get("https://intern-monitoring.github.io/js/data.json", isiDataProposal);
+get(
+  "https://intern-monitoring.github.io/proposal/js/data.json",
+  isiDataProposal
+);
